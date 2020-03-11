76550bc147b34acf4c9edb141f01651292161658
Jefferson Felizardo <jeffer1312@gmail.com>
2020-03-11 16:35:46 -0300

    Promisse

---
+++ esnext/promisses.js
@@ -0,0 +1,12 @@
+function falarDepoisDe(segundos, frase) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(frase)
+        }, segundos * 1000)
+    })
+
+}
+
+falarDepoisDe(3, 'Funcionou')
+    .then(frase => frase.concat('?!?'))
+    .then(outrafrase => console.log(outrafrase))
